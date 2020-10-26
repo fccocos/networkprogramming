2ce5f17f9819d1522064093c09c59fe335ab3d5b
fccocos <47286937+fccocos@users.noreply.github.com>
2020-10-26 15:20:58 +0800

    Create CTcpTran.h

---
+++ CTcpTran.h
@@ -0,0 +1,27 @@
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+#define SOCKETBIND 1
+#define SOCKETNOBIND 2
+#define SOKET_TIMEOUT -100
+
+#include "winsock2.h"
+
+#include <winsock.h>
+#include <string>
+using namespace std;
+#pragma comment(lib,"ws2_32.lib")
+class CTcpTran{
+public:
+  CTcpTran();
+  virtual ~CTcpTran();
+public:
+  staic BOOL InitSocketLibrary(int lower,int higver);
+public:
+  SOCKET InitSocket(int SocketType,string strBindIp,u_short BindPort);
+  int mysend(SOCKET sock,const char *buf,int len, int flag,int overtime);
+  int myrecv(SOCKET sock,char *buf,int len,int flag,int overtime,char* EndMark,Bool soonflag=FALSE);
+private:
+  SOCKET m_Socket;
+};
